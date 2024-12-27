7d3d69356be5e26024f0ce0c6fb9011ad074736e
Ferid Kazimli <ferid.kazimli@gmail.com>
2024-12-27 23:09:56 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,14 +7,14 @@ export default function Home() {
         <div className="container">
 
             <Head>
-                <title>Privacy Policy for Line Shot - Basket </title>
+                <title>Privacy Policy for Round The World </title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
             <main>
-                <Header title="Privacy Policy for Line Shot - Basket" />
+                <Header title="Privacy Policy for Round The World" />
                 <p className="description">
-                    <p> Camanbay built the Line Shot - Basket - Söz Oyunu app as
+                    <p> Camanbay built the Round The World - Söz Oyunu app as
                         a Free app. This SERVICE is provided by
                         Camanbay at no cost and is intended for use as
                         is.
@@ -34,7 +34,7 @@ export default function Home() {
                     <p>
                         The terms used in this Privacy Policy have the same meanings
                         as in our Terms and Conditions, which are accessible at
-                        Line Shot - Basket - Söz Oyunu unless otherwise defined in this Privacy Policy.
+                        Round The World - Söz Oyunu unless otherwise defined in this Privacy Policy.
                     </p>
                     <p><strong>Information Collection and Use</strong></p>
                     <p>
